8f1647315d3a2f318bedff605863be90bb446081
windyakin <windyakin@gmail.com>
2017-12-24 22:00:05 +0900

    :movie_camera: OMX Playerで動画を流せるようにする

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const Dasher = require('node-dash-button');
+const OmxManager = require('omx-manager');
 const Dotenv = require('dotenv');
 
 Dotenv.config({ path: './.env' });
 const Dash = Dasher(process.env.DASH_BUTTON_MAC_ADDRESS, null, null, 'all');
 
+const omxPlayer = new OmxManager({ position: '0,0,800,400' });
 Dash.on('detected', () => {
   console.log('detected');
+  const player = omxPlayer.create('video/sample.mp4', { '--win': '0,0,800,480' });
+  player.play();
+  player.on('end', () => {
+    console.log('Video play end');
+  });
 });
