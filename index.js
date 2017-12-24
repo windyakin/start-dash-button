95cfbef2bc88f303744218a793dadb08cfde2917
windyakin <windyakin@gmail.com>
2017-12-24 21:55:58 +0900

    :cop: Fix by lint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Dasher = require('node-dash-button');
+const Dotenv = require('dotenv');
 
-require('dotenv').config({ path: './.env' });
+Dotenv.config({ path: './.env' });
 const Dash = Dasher(process.env.DASH_BUTTON_MAC_ADDRESS, null, null, 'all');
 
 Dash.on('detected', () => {
